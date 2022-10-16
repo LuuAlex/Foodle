645199c258ace8f1a4d29b676e89658fe4983d7f
Alex Luu <103136100+LuuAlex@users.noreply.github.com>
2022-10-16 03:47:11 -0700

    added get_user_data function

--- test_and_helper/add_get_database.py
+++ test_and_helper/add_get_database.py
@@ -1,6 +1,8 @@
 from .database_access import *
 cur, conn = get_database()
 
+# ROW = [user_id, date, sex, age, height, weight, caloric_intake, protein_grams_intake, carb_grams_intake, fat_grams_intake]
+
 def add_entry(user_id, date, sex, age, height, weight, caloric_intake, protein_grams_intake, carb_grams_intake, fat_grams_intake):
     cur.execute("""INSERT INTO people.data VALUES 
         (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
@@ -11,6 +13,11 @@ def get_user_info(user_id):
     cur.execute(f"SELECT * FROM people.data WHERE user_id='{user_id}'")
     row = cur.fetchall()[0]
     return row[2], row[3], row[4], row[5]
+    
+def get_user_data(user_id):
+    cur.execute(f"SELECT * FROM people.data WHERE user_id='{user_id}'")
+    rows = cur.fetchall()
+    return [[row[1], row[6], row[7], row[8], row[9]] for row in rows]
 
 def get_intake(food, column_int):
     cur.execute(f"SELECT * FROM nutrients.data WHERE food='{food}'")
