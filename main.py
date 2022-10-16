9904efe5171fac10cfb289182b78222d5e8ba47c
Vrushank Prakash <115768729+vprak17@users.noreply.github.com>
2022-10-16 07:04:27 -0700

    main update

--- main.py
+++ main.py
@@ -5,14 +5,21 @@ from test_and_helper.add_get_database import *
 username = input('Please type your name to start.')
 
 user = input('''Welcome back to Foodle! Please choose what you would like to view today.
-                Type 1 to view a table of your daily intakes
-                Type 2 to view a table that compares your daily intakes to the recommended intakes
+                Type 1 to add an entry of your daily intakes
+                Type 2 to view a table of your daily intakes
                 Type 3 to view a graph of the trends of your daily intakes
-                Type 4 to add an entry of your daily intakes
                  ''')
 
 
-# while user in [1, 2, 3, 4]:
-#     if user == 1:
-#         data = [[]]
-#         for i in 
+while user in [1, 2, 3]:
+    if user == 1:
+        data = [[]]
+        for i in 
+
+    elif user == 2:
+        fi
+    
+    elif user == 3:
+
+    else:
+        
