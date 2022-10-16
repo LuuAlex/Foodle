156e4d0ee121a015dd1a9f2c6651a9b6f3a30642
Alex Luu <103136100+LuuAlex@users.noreply.github.com>
2022-10-16 07:54:19 -0700

    added type 2

--- add_user_entry.py
+++ add_user_entry.py
@@ -4,11 +4,16 @@ from datetime import date
 from test_and_helper.database_access import *
 from test_and_helper.add_get_database import *
 
-def run():
-    username = input('Please enter your name: ')
+def run(username=None):
+    if username == None:
+        username = input('Please enter your name: ')
     info = get_user_info(username)
     if info:
         curr_date = date.today()
+        user_sex = info[0]
+        user_age = info[1]
+        user_height = info[2]
+        user_weight = info[3]
     else:
         user_weight = input('Please enter your weight (lbs): ')
         user_height = input('Please enter your height (inches): ')
@@ -17,7 +22,7 @@ def run():
         curr_date = date.today()
 
     foods = {}
-    print('Please enter the items you have eaten today and the quantity. Please type done when you are finished. Example: 3 carrots')
+    print('Please enter the items you have eaten today and the quantity. Please type "done" when you are finished. Example: 3 Carrots')
     food = input()
     while food.lower() != 'done':
         food = food.split()
@@ -27,27 +32,25 @@ def run():
     def return_total_cals(foods):
         total_cals = 0
         for i in foods.keys():
-            total_cals += (foods[i] * get_intake(i, 3))
+            total_cals += (int(foods[i]) * float(get_intake(i, 3)))
         return total_cals
 
     def return_total_protein(foods):
         total_protein = 0
         for i in foods.keys():
-            total_protein += (foods[i] * get_intake(i, 4))
+            total_protein += (int(foods[i]) * float(get_intake(i, 4)))
         return total_protein
 
     def return_total_fat(foods):
         total_fat = 0
         for i in foods.keys():
-            total_fat += (food[i] * get_intake(i, 5))
+            total_fat += (int(foods[i]) * float(get_intake(i, 5)))
         return total_fat
 
     def return_total_carbs(foods):
         total_carbs = 0
         for i in foods.keys():
-            total_carbs += (food[i] * get_intake(i, 8))
+            total_carbs += (int(foods[i]) * float(get_intake(i, 8)))
         return total_carbs
 
     add_entry(username, curr_date, user_sex, user_age, user_height, user_weight, return_total_cals(foods), return_total_protein(foods), return_total_carbs(foods), return_total_fat(foods))
-
-run()
--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 # import stuff
 from tabulate import tabulate
 from test_and_helper.add_get_database import *
-from add_user_entry import *
+import add_user_entry
 
-username = input('Please type your name to start.')
+username = input('Please type your name to start: ')
 
-user = input('''Welcome back to Foodle! Please choose what you would like to view today.
+user = int(input('''Welcome back to Foodle! Please choose what you would like to view today.
                 Type 1 to add an entry of your daily intakes
                 Type 2 to view a table of your daily intakes
                 Type 3 to view a graph of the trends of your daily intakes
-                 ''')
+                 '''))
 
 
 while user in [1, 2, 3]:
     if user == 1:
-        run()
+        add_user_entry.run(username)
+        user = -1
 """
     elif user == 2:
         fi
