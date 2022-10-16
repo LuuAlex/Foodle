5b9e5d57c1480b61d72c0fcece6df6de9262aa5c
Vrushank Prakash <vprak17@Vrushanks-MacBook-Air.local>
2022-10-15 19:14:10 -0700

    added main and nutrients file

---
+++ main.py
@@ -0,0 +1,12 @@
+import math
+import pandas
+
+name = input("Please enter your name: ")
+weight = input("Weight: ")
+height = input("Height: ")
+age = input("Age: ")
+sex = input("Sex: ")
+weight = input("Weight: ")
+weight = input("Weight: ")
+
+def compares_rec_to_person(name, )
